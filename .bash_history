fa2a82a1b20ea733de31cf4c00e17ae167b33047
Kamil Ślósarz <kamil.slosarz@netart.pl>
2018-05-30 09:41:44 +0200

    fixed untracked files

---
+++ .bash_history
@@ -0,0 +1,60 @@
+exot
+exit
+exit
+exit
+exit
+ls
+cklear
+exit
+cd ..
+cd devops/
+cd ../
+cd /
+ls
+ls -la
+cd bar
+cd home
+ks
+ls
+cd ..
+cd var/
+ls
+cd www/
+ls
+rm -rf html/
+sudo rm -rf html/
+rm -rf html/
+cd ..
+cd www/devops/
+l;s
+ls
+vendor/bin/phpunit -c tests/phpunit/phpunit.xml 
+vendor/bin/phpunit -c tests/phpunit/phpunit.xml 
+cd vendor/
+ls
+cd ..
+composer install
+composer install
+sudo apt-get intall composer
+sudo apt-get intall composer
+sudo apt-get install composer
+exit
+ks
+ls
+ls -la
+cd ..
+ls
+cd ..
+ls
+cd ..
+ls
+ls -la
+ls
+cd home/
+ls
+mkdir test
+cd ..
+cd /var/www/devops/
+ls
+ls -la
+exit
---
+++ tests/fixture/default.sql
@@ -0,0 +1,95 @@
+
+-----------------------------------------------------------------------
+-- projects
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [projects];
+
+CREATE TABLE [projects]
+(
+    [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    [name] VARCHAR(255) NOT NULL,
+    [repository] VARCHAR(255) NOT NULL,
+    [created_at] TIMESTAMP,
+    [updated_at] TIMESTAMP,
+    UNIQUE ([id])
+);
+
+-----------------------------------------------------------------------
+-- roles
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [roles];
+
+CREATE TABLE [roles]
+(
+    [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    [name] VARCHAR(255) NOT NULL,
+    UNIQUE ([id])
+);
+
+-----------------------------------------------------------------------
+-- users
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [users];
+
+CREATE TABLE [users]
+(
+    [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    [username] VARCHAR(255) NOT NULL,
+    [password] VARCHAR(255) NOT NULL,
+    [created_at] TIMESTAMP,
+    [updated_at] TIMESTAMP,
+    UNIQUE ([id])
+);
+
+-----------------------------------------------------------------------
+-- users_auth_tokens
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [users_auth_tokens];
+
+CREATE TABLE [users_auth_tokens]
+(
+    [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    [user_id] INTEGER,
+    [token] VARCHAR(255) NOT NULL,
+    [created_at] TIMESTAMP,
+    [updated_at] TIMESTAMP,
+    UNIQUE ([id]),
+    FOREIGN KEY ([user_id]) REFERENCES [users] ([id])
+);
+
+CREATE INDEX [users_auth_tokens_i_6ca017] ON [users_auth_tokens] ([user_id]);
+
+-----------------------------------------------------------------------
+-- users_roles
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [users_roles];
+
+CREATE TABLE [users_roles]
+(
+    [user_id] INTEGER NOT NULL,
+    [role_id] INTEGER NOT NULL,
+    PRIMARY KEY ([user_id],[role_id]),
+    UNIQUE ([user_id],[role_id]),
+    FOREIGN KEY ([user_id]) REFERENCES [users] ([id]),
+    FOREIGN KEY ([role_id]) REFERENCES [roles] ([id])
+);
+
+-----------------------------------------------------------------------
+-- users_privileges
+-----------------------------------------------------------------------
+
+DROP TABLE IF EXISTS [users_privileges];
+
+CREATE TABLE [users_privileges]
+(
+    [id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    [user_id] INTEGER,
+    [name] VARCHAR(255) NOT NULL,
+    UNIQUE ([id]),
+    FOREIGN KEY ([user_id]) REFERENCES [users] ([id])
+);
